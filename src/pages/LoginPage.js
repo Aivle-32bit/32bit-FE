0d66b9d3ad553301d7ffa84b203c753fcb5f7398
curiousking <ljw1002kr@naver.com>
2024-07-10 17:28:51 +0900

    Front

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 import './LoginPage.css';
 import Login from '../components/Login';
