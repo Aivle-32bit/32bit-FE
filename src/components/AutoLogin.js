2c84deb169b27ce5790962d6f336c7b6b03124c1
curiousking <87610232+cu-ree@users.noreply.github.com>
2024-07-12 17:24:56 +0900

    Delete src directory

--- src/components/AutoLogin.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './AutoLogin.css';
-
-const AutoLogin = () => {
-  return <div className="auto-login-wrapper">자동 로그인</div>;
-};
-
-export default AutoLogin;
--- src/components/SaveId.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './SaveId.css';
-
-
-const SaveId = () => {
-  return <div className="save-id-wrapper">아이디 저장</div>;
-};
-
-export default SaveId;
