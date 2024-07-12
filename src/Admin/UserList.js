dc170ce9b048d3f77c9353ee5b280b99923e4396
curee <ljw1002kr@naver.com>
2024-07-12 17:47:09 +0900

    add

--- src/Admin/UserList.js
+++ src/Admin/UserList.js
@@ -9,8 +9,8 @@ const UserList = () => {
     const dispatch = useDispatch();
     const users = useSelector(state => state.user.users);
     const [currentPage, setCurrentPage] = useState(1);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [modalAction, setModalAction] = useState('');
+    // const [selectedUser, setSelectedUser] = useState(null); // 사용하지 않는 변수
+    // const [modalAction, setModalAction] = useState(''); // 사용하지 않는 변수
     const usersPerPage = 5;
 
     useEffect(() => {
@@ -28,7 +28,7 @@ const UserList = () => {
         } catch (error) {
             alert('유저를 휴면 상태로 전환하는데 실패했습니다.');
         }
-        setSelectedUser(null);
+        // setSelectedUser(null); // 사용하지 않는 함수
     };
 
     const handleDeleteUser = (userId) => {
@@ -38,19 +38,19 @@ const UserList = () => {
         } catch (error) {
             alert('유저 탈퇴에 실패했습니다.');
         }
-        setSelectedUser(null);
+        // setSelectedUser(null); // 사용하지 않는 함수
     };
 
     const handleAdminRegister = (userId) => {
         console.log(`User with ID ${userId} has been registered as admin`);
         // 여기서 실제 등록 로직을 구현하세요
-        setSelectedUser(null);
+        // setSelectedUser(null); // 사용하지 않는 함수
     };
 
     const handleAdminDeregister = (userId) => {
         console.log(`User with ID ${userId} has been deregistered as admin`);
         // 여기서 실제 해제 로직을 구현하세요
-        setSelectedUser(null);
+        // setSelectedUser(null); // 사용하지 않는 함수
     };
 
     const startIndex = (currentPage - 1) * usersPerPage;
@@ -82,13 +82,13 @@ const UserList = () => {
                             {/* 추후 슈퍼 관리자에게만 보이도록 설정 */}
                             <td>
                                 {user.is_admin ? (
-                                    <button onClick={() => { setSelectedUser(user); setModalAction('deregister'); }} className="action-button">해제</button>
+                                    <button onClick={() => { /* setSelectedUser(user); setModalAction('deregister'); */ }} className="action-button">해제</button>
                                 ) : (
-                                    <button onClick={() => { setSelectedUser(user); setModalAction('register'); }} className="action-button">등록</button>
+                                    <button onClick={() => { /* setSelectedUser(user); setModalAction('register'); */ }} className="action-button">등록</button>
                                 )}
                             </td>
                             <td>
-                                <button onClick={() => { setSelectedUser(user); setModalAction('delete'); }} className="action-button">탈퇴</button>
+                                <button onClick={() => { /* setSelectedUser(user); setModalAction('delete'); */ }} className="action-button">탈퇴</button>
                             </td>
                         </tr>
                     ))}
