d5a8894548bd369fd5ba191303f251987c323b76
seoyoung <winckey9625@gmail.com>
2024-07-17 14:02:41 +0900

    로그인 하지 않은 유저는 analysis와 report가 보이지 않도록 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,30 +25,34 @@ const Navbar = () => {
   };
 
   return (
-      <nav className={getNavbarClass()}>
-        <div className="navbar-left">
-          <Link to="/" className={location.pathname === '/' ? 'active-home' : ''}>HOME</Link>
-          <Link to="/about-us" className={isActive('/about-us') ? 'active' : ''}>ABOUT US</Link>
-          <Link to="/notice" className={isActive('/notice') ? 'active' : ''}>NOTICE</Link>
-          <Link to="/analysis" className={isActive('/analysis') ? 'active' : ''}>ANALYSIS</Link>
-          <Link to="/report" className={isActive('/report') ? 'active' : ''}>REPORT</Link>
-        </div>
-        <div className="navbar-right">
-          {!isLoggedIn ? (
-              <>
-                <Link to="/login" className={isActive('/login') ? 'active' : ''}>LOGIN</Link>
-                <Link to="/signup" className={isActive('/signup') ? 'active' : ''}>SIGN UP</Link>
-              </>
-          ) : (
-              <>
-                <span className="welcome-message">안녕하세요 {user.name}님.</span>
-                <Link to="/mypage/profile" className={isActive('/mypage') ? 'active' : ''}>MY PAGE</Link>
-                {user.isAdmin && <Link to="/admin/manage-user" className={isActive('/admin') ? 'active' : ''}>ADMIN</Link>}
-                <Link to="/" onClick={handleLogout} className="logout-link">LOGOUT</Link>
-              </>
-          )}
-        </div>
-      </nav>
+    <nav className={getNavbarClass()}>
+      <div className="navbar-left">
+        <Link to="/" className={location.pathname === '/' ? 'active-home' : ''}>HOME</Link>
+        <Link to="/about-us" className={isActive('/about-us') ? 'active' : ''}>ABOUT US</Link>
+        <Link to="/notice" className={isActive('/notice') ? 'active' : ''}>NOTICE</Link>
+        {isLoggedIn && (
+          <>
+            <Link to="/analysis" className={isActive('/analysis') ? 'active' : ''}>ANALYSIS</Link>
+            <Link to="/report" className={isActive('/report') ? 'active' : ''}>REPORT</Link>
+          </>
+        )}
+      </div>
+      <div className="navbar-right">
+        {!isLoggedIn ? (
+          <>
+            <Link to="/login" className={isActive('/login') ? 'active' : ''}>LOGIN</Link>
+            <Link to="/signup" className={isActive('/signup') ? 'active' : ''}>SIGN UP</Link>
+          </>
+        ) : (
+          <>
+            <span className="welcome-message">안녕하세요 {user.name}님.</span>
+            <Link to="/mypage/profile" className={isActive('/mypage') ? 'active' : ''}>MY PAGE</Link>
+            {user.isAdmin && <Link to="/admin/manage-user" className={isActive('/admin') ? 'active' : ''}>ADMIN</Link>}
+            <Link to="/" onClick={handleLogout} className="logout-link">LOGOUT</Link>
+          </>
+        )}
+      </div>
+    </nav>
   );
 };
 
