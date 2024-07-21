60c83ece9fb6e2a9e4effa2893a375ed7c7c492e
seoyoung <winckey9625@gmail.com>
2024-07-21 23:57:53 +0900

    반응형 레이아웃 추가

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,9 +5,11 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
-      <div className="financial-detective">
-        FINANCIAL<br />
-        &nbsp;&nbsp;DETECTIVE
+      <div className="home-content">
+        <span className="home-title">
+          FINANCIAL<br />
+          &nbsp;&nbsp;DETECTIVE
+        </span>
       </div>
     </div>
   );
