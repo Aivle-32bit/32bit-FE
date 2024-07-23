1742881afd695d13fa770e6bd58a3e531563c92a
pwrwpw <dlgustjr9912@gmail.com>
2024-07-23 13:56:48 +0900

    보고서 생성 AI API 추가

--- src/pages/Admin/ManageCompany.js
+++ src/pages/Admin/ManageCompany.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { get_all_company } from '../../api';
+import React, {useEffect, useState} from 'react';
+import {createCompanyReport, get_all_company} from '../../api';
 import './ManageCompany.css';
 
 const ManageCompany = () => {
   const [companies, setCompanies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [alertType, setAlertType] = useState('');
   const companiesPerPage = 5;
 
   useEffect(() => {
@@ -22,7 +24,8 @@ const ManageCompany = () => {
   // 페이지네이션 로직
   const indexOfLastCompany = currentPage * companiesPerPage;
   const indexOfFirstCompany = indexOfLastCompany - companiesPerPage;
-  const currentCompanies = companies.slice(indexOfFirstCompany, indexOfLastCompany);
+  const currentCompanies = companies.slice(indexOfFirstCompany,
+      indexOfLastCompany);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -31,8 +34,40 @@ const ManageCompany = () => {
     console.log(`ID가 ${companyId}인 회사를 삭제합니다.`);
   };
 
+  const handleFileChange = async (e, companyId) => {
+    const file = e.target.files[0];
+    if (file && file.type === 'text/csv') {
+      await handleFileUpload(file, companyId);
+    } else {
+      setAlertMessage('CSV 파일만 업로드할 수 있습니다.');
+      setAlertType('error');
+    }
+  };
+
+  const handleFileUpload = async (file, companyId) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await createCompanyReport(companyId, formData);
+      setAlertMessage('파일 업로드 성공!');
+      setAlertType('success');
+      console.log('파일 업로드 성공:', response);
+    } catch (error) {
+      setAlertMessage('파일 업로드 실패!');
+      setAlertType('error');
+      console.error('파일 업로드 실패:', error);
+    }
+  };
+
   return (
       <div className="company-container">
+        {alertMessage && (
+            <div className={`alert ${alertType === 'success' ? 'alert-success'
+                : 'alert-error'}`}>
+              {alertMessage}
+            </div>
+        )}
         <div className="company-card">
           <div className="company-table-title">◾️ 등록 회사 관리</div>
           <table className="company-table">
@@ -53,14 +88,22 @@ const ManageCompany = () => {
                   <td>{company.businessType}</td>
                   <td>
                     <div className="input-file-container">
-                      <input type="file" id={`file-${company.id}`} className="input-file" />
-                      <label htmlFor={`file-${company.id}`} className="input-file-trigger">
+                      <input
+                          type="file"
+                          id={`file-${company.id}`}
+                          className="input-file"
+                          onChange={(e) => handleFileChange(e, company.id)}
+                      />
+                      <label htmlFor={`file-${company.id}`}
+                             className="input-file-trigger">
                         파일 선택
                       </label>
                     </div>
                   </td>
                   <td>
-                    <button className="delete-button" onClick={() => handleDelete(company.id)}>삭제</button>
+                    <button className="delete-button"
+                            onClick={() => handleDelete(company.id)}>삭제
+                    </button>
                   </td>
                 </tr>
             ))}
@@ -77,7 +120,12 @@ const ManageCompany = () => {
   );
 };
 
-const Pagination = ({ companiesPerPage, totalCompanies, paginate, currentPage }) => {
+const Pagination = ({
+  companiesPerPage,
+  totalCompanies,
+  paginate,
+  currentPage
+}) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalCompanies / companiesPerPage); i++) {
@@ -88,8 +136,11 @@ const Pagination = ({ companiesPerPage, totalCompanies, paginate, currentPage })
       <nav className="pagination-nav">
         <ul className="pagination">
           {pageNumbers.map(number => (
-              <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
-                <a onClick={() => paginate(number)} className="page-link" href="#!">
+              <li key={number}
+                  className={`page-item ${currentPage === number ? 'active'
+                      : ''}`}>
+                <a onClick={() => paginate(number)} className="page-link"
+                   href="#!">
                   {number}
                 </a>
               </li>
