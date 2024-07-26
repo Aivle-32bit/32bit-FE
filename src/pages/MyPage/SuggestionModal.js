3b0ea0e09904d60d96ef5a18cb0b9de77ba74cbf
pwrwpw <dlgustjr9912@gmail.com>
2024-07-26 09:21:34 +0900

    관리자만 답글 버튼이 보이도록 수정

--- src/pages/MyPage/SuggestionModal.js
+++ src/pages/MyPage/SuggestionModal.js
@@ -16,7 +16,9 @@ const SuggestionModal = ({
   const [isSecret, setIsSecret] = useState(suggestion.isSecret);
 
   useEffect(() => {
-
+    setUpdatedTitle(suggestion.title);
+    setUpdatedContent(suggestion.content);
+    setIsSecret(suggestion.isSecret);
   }, [suggestion]);
 
   const toggleEditMode = () => {
@@ -36,8 +38,11 @@ const SuggestionModal = ({
   };
 
   const handleSaveChanges = () => {
-    handleUpdateSuggestion(suggestion.boardId,
-        {title: updatedTitle, content: updatedContent, isSecret});
+    handleUpdateSuggestion(suggestion.boardId, {
+      title: updatedTitle,
+      content: updatedContent,
+      isSecret,
+    });
     toggleEditMode();
   };
 
@@ -47,56 +52,105 @@ const SuggestionModal = ({
              onClick={(e) => e.stopPropagation()}>
           {editMode ? (
               <>
-                <span className='suggetion-modal-edit-title'>건의사항 수정</span>
-                <span className='suggetion-modal-edit-label'>제목</span>
-                <input className='suggetion-modal-edit-input' type="text" value={updatedTitle}
-                       onChange={handleTitleChange}/>
-                <span className='suggetion-modal-edit-label'>내용</span>
-                <textarea className='suggetion-modal-edit-input' value={updatedContent}
-                          onChange={handleContentChange}></textarea>
-                <div className='suggetion-modal-edit-action-area'>
-                  <div className='suggetion-modal-edit-secret-area'>
+                <span className="suggestion-modal-edit-title">건의사항 수정</span>
+                <span className="suggestion-modal-edit-label">제목</span>
+                <input
+                    className="suggestion-modal-edit-input"
+                    type="text"
+                    value={updatedTitle}
+                    onChange={handleTitleChange}
+                />
+                <span className="suggestion-modal-edit-label">내용</span>
+                <textarea
+                    className="suggestion-modal-edit-input"
+                    value={updatedContent}
+                    onChange={handleContentChange}
+                ></textarea>
+                <div className="suggestion-modal-edit-action-area">
+                  <div className="suggestion-modal-edit-secret-area">
                     <label>
-                      <input className='suggetion-modal-edit-secret' type="checkbox" checked={isSecret}
-                            onChange={handleIsSecretChange}/> 비밀글
+                      <input
+                          className="suggestion-modal-edit-secret"
+                          type="checkbox"
+                          checked={isSecret}
+                          onChange={handleIsSecretChange}
+                      /> 비밀글
                     </label>
                   </div>
                   <div className="suggestion-modal-button-container">
-                    <button className="suggestion-modal-edit-save-button" onClick={handleSaveChanges}>저장</button>
-                    <button className="suggestion-modal-edit-cancel-button" onClick={toggleEditMode}>취소</button>
+                    <button
+                        className="suggestion-modal-edit-save-button"
+                        onClick={handleSaveChanges}
+                    >
+                      저장
+                    </button>
+                    <button
+                        className="suggestion-modal-edit-cancel-button"
+                        onClick={toggleEditMode}
+                    >
+                      취소
+                    </button>
                   </div>
                 </div>
               </>
           ) : (
               <>
-                <span className='suggestion-modal-title'>{suggestion.title}</span>
-                <span className='suggestion-modal-writer'>작성자{'\u00A0'.repeat(9)}{suggestion.memberName}</span>
-                <span className='suggestion-modal-date'>작성일시{'\u00A0'.repeat(5)}{new Date(suggestion.createdAt).toLocaleString()}</span>
-                <span className='suggestion-modal-text'>{suggestion.content}</span>
+                <span
+                    className="suggestion-modal-title">{suggestion.title}</span>
+                <span className="suggestion-modal-writer">
+              작성자{' '.repeat(9)}{suggestion.memberName}
+            </span>
+                <span className="suggestion-modal-date">
+              작성일시{' '.repeat(5)}{new Date(
+                    suggestion.createdAt).toLocaleString()}
+            </span>
+                <span
+                    className="suggestion-modal-text">{suggestion.content}</span>
                 <div className="suggestion-modal-button-container">
-                  {suggestion.parentId === null && (
-                    <button className="suggestion-modal-reply-button" onClick={(e) => {
-                      e.stopPropagation();
-                      handleReplyClick(suggestion.boardId, false); // Pass false for normal replies
-                    }}>답글 달기</button>
+                  {isAdmin && suggestion.parentId === null && (
+                      <button
+                          className="suggestion-modal-reply-button"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleReplyClick(suggestion.boardId, false); // Pass false for normal replies
+                          }}
+                      >
+                        답글 달기
+                      </button>
                   )}
                   {userId === suggestion.memberId && (
-                    <>
-                      <button className="suggestion-modal-edit-button" onClick={toggleEditMode}>수정</button>
-                      <button className="suggestion-modal-delete-button" onClick={() => handleDeleteSuggestion(
-                          suggestion.boardId)}>삭제
-                      </button>
-                    </>
+                      <>
+                        <button
+                            className="suggestion-modal-edit-button"
+                            onClick={toggleEditMode}
+                        >
+                          수정
+                        </button>
+                        <button
+                            className="suggestion-modal-delete-button"
+                            onClick={() => handleDeleteSuggestion(
+                                suggestion.boardId)}
+                        >
+                          삭제
+                        </button>
+                      </>
                   )}
                   {isAdmin && userId !== suggestion.memberId && (
-                    <button className="suggestion-modal-delete-button" onClick={() => handleDeleteSuggestion(
-                        suggestion.boardId)}>삭제
-                    </button>
+                      <button
+                          className="suggestion-modal-delete-button"
+                          onClick={() => handleDeleteSuggestion(
+                              suggestion.boardId)}
+                      >
+                        삭제
+                      </button>
                   )}
                 </div>
               </>
           )}
-          <button className="suggestion-modal-close-button" onClick={closeModal}>확인</button>
+          <button className="suggestion-modal-close-button"
+                  onClick={closeModal}>
+            확인
+          </button>
         </div>
       </div>
   );
